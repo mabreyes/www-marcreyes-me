475fb4d97623c96fd21790772c84fc929969f051
Marc Reyes <marcreyesmedia@gmail.com>
2017-01-01 18:27:48 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -28,20 +28,6 @@
 	------------------------------------------------------ */
   	$(".fluid-video-wrapper").fitVids();
 
-	/* Counter */
-		$('.count').each(function () {
-		    $(this).prop('Counter',0).animate({
-		        Counter: $(this).text()
-		    }, {
-		        duration: 2500,
-		        easing: 'swing',
-		        step: function (now) {
-		            $(this).text(Math.ceil(now));
-		        }
-		    });
-		});
-
-
 	/* --------------------------------------------------- */
 	/*  Vegas Slideshow
 	------------------------------------------------------ */
