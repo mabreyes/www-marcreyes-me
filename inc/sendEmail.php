e7c4f7800d3dbb2913441afeeff1d4af8ee81a65
Marc Reyes <marcreyesmedia@gmail.com>
2017-01-26 16:13:17 +0800

    E-mail forwarding PHP updated

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -1,6 +1,6 @@
 ﻿<?php
 
-// Replace this with your own email address
+// Replace this with your own email address, address updated to match
 $siteOwnersEmail = 'marcreyesmedia@gmail.com';
 
 
@@ -51,7 +51,7 @@ if($_POST) {
 
 		if ($mail) { echo "OK"; }
       else { echo "Something went wrong. Please try again."; }
-		
+
 	} # end if - no validation error
 
 	else {
@@ -59,7 +59,7 @@ if($_POST) {
 		$response = (isset($error['name'])) ? $error['name'] . "<br /> \n" : null;
 		$response .= (isset($error['email'])) ? $error['email'] . "<br /> \n" : null;
 		$response .= (isset($error['message'])) ? $error['message'] . "<br />" : null;
-		
+
 		echo $response;
 
 	} # end if - there was a validation error
